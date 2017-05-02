db383aea39091151a72f03af4a1bfb9d8ad39864
Younes <yoonux@gmail.com>
2017-05-02 16:30:58 +0100

    update pagination 100%

--- src/main/java/hello/ArticleController.java
+++ src/main/java/hello/ArticleController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +16,9 @@ public class ArticleController {
 	private ArticleRepository articleRepository;
 	
 	@RequestMapping(path="/articles")
-	public @ResponseBody List<Article> findAllArticles() {
-		return articleRepository.getFiltredList("", "", "-1", 10, 0);
-	}
-	
-	@RequestMapping(path="/articles/{filter}")
-	public @ResponseBody List<Article> findAllArticles(@PathVariable String filter) {
-		return articleRepository.getFiltredList(filter, "", "-1", 10, 0);
+	public @ResponseBody List<Article> allArticles(@RequestParam(value="filter", defaultValue="") String filter, 
+			@RequestParam(value="pageIndex", defaultValue="0") Integer pageIndex) {
+		return articleRepository.getFiltredList(filter, "", "-1", 20, pageIndex);
 	}
 
 }
